ba623fa550196b92256edccb4f4a80e9352d3fbf
Max Franke <max.franke@acad.pucrs.br>
2020-06-03 01:19:04 -0300

    ????

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors());
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello World!</h1>')
 })
+
+app.post('/', function (req, res) {
+    console.log(req.body.nome)
+});
 
 const port = 5000;
 app.listen(port, () => `Server running on port ${port}`);
