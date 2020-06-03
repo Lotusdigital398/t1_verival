be9a26dcf4a26647de93ab4dc8b66ef46154580c
Lotusdigital <gabriel.silva.010@acad.pucrs.br>
2020-06-03 02:59:33 -0300

    React Router adicionado

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Calendar from "./CalendarComponent";
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Calendar/>
-    </div>
-  );
-}
+import CalendarComponent from "./component/CalendarComponent";
+import Home from "./component/Home";
+import Error from "./component/Error";
+import Navigation from "./component/Navigation";
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Navigation />
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                        <Route path="/calendar" component={CalendarComponent} />
+                        <Route component={Error} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+};
 
 export default App;
