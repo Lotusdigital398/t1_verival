d1ff2cdb531f78ae81be6b0306f6220063bbb6dd
Lotusdigital <gabriel.silva.010@acad.pucrs.br>
2020-06-07 20:07:54 -0300

    Tentativa de implementação de um Modal

--- src/client/component/CalendarComponent.js
+++ src/client/component/CalendarComponent.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import {Calendar, momentLocalizer} from "react-big-calendar";
 import moment from "moment";
+import Modal from '@material-ui/core/Modal';
+
+
 
 import './css/Calendar.css'
 
@@ -15,10 +18,12 @@ class CalendarComponent extends Component {
         super(props);
         this.state = {
             events: [],
-            reservas: []
+            reservas: {},
+            open: false
         }
         this.newEvent = this.newEvent.bind(this)
         this.getReservas = this.getReservas.bind(this)
+        this.test = this.test.bind(this)
     }
 
     componentDidMount() {
@@ -32,6 +37,7 @@ class CalendarComponent extends Component {
                 'Content-Type': 'application/json'
             }
         }).then(res => res.text()).then(res => {
+            this.setState({event: JSON.parse(res)})
             JSON.parse(res).forEach(item => {
                 this.newEvent(item);
             })
@@ -42,8 +48,13 @@ class CalendarComponent extends Component {
 
         let idList = this.state.events.map(a => a.id)
         let newId = Math.max(...idList) + 1
+        var msg = event.recurso === 'sala' ? "Quantidade de Assentos: " : "Quantidade: ";
         let tipo = event.tipo.charAt(0).toUpperCase() + event.tipo.slice(1)
-        let titulo = "Colaborador: " +  event.nome + " (" + event.matricula + ") " + "Reserva: " + tipo + " (" + parseInt(event.preco).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) + ")";
+        let rec = event.recurso.charAt(0).toUpperCase() + event.recurso.slice(1)
+        let titulo = "Colaborador: " + event.nome + " (" + event.matricula + ") " + "Reserva de " + rec + ": " + tipo + " " + msg + event.quantidade + " (" + parseInt(event.preco).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }) + ")";
         let hour = {
             id: newId,
             title: titulo,
@@ -55,17 +66,44 @@ class CalendarComponent extends Component {
         })
     }
 
+    test(event) {
+        console.log("SAUIHHUDSA")
+        this.handleOpen()
+    }
+
+    handleOpen() {
+        this.setState({open: true});
+    }
+
+    handleClose() {
+        this.setState({open: false});
+    }
 
     render() {
         return (
+            <Modal
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={this.state.open}
+                onClose={this.handleClose}
+            >
+                <div >
+                    <h2>Simple React Modal</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi accumsan odio enim, non pharetra est ultrices et.
+                    </p>
+                </div>
+            </Modal>,
             <div className="Calendar">
                 <Calendar
+                    popup
                     views={['month', 'agenda']}
                     localizer={localizer}
                     defaultDate={new Date()}
                     defaultView="month"
                     events={this.state.events}
                     style={{height: "100vh"}}
+                    onSelectEvent={this.test}
                 />
             </div>
         );
