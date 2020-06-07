10cc14bbfed8317d750e86db3813367c3aa434c0
Lucas Schell <lucas.schell@acad.pucrs.br>
2020-06-07 02:11:11 -0300

    remove linhas desnecessarias

--- src/client/pages/Reserva.js
+++ src/client/pages/Reserva.js
@@ -110,24 +110,20 @@ class Reserva extends Component {
 
     handleDataI(event) {
         this.setState({dataI: event})
+        let dif;
         if (this.state.recurso === "mobilia" && this.state.dataF.getTime() < (event.getTime() + 4 * 86400000)) {
-            this.setState({dataF: (event.getTime() + 4 * 86400000)})
-
-            const dif = Math.ceil(((event.getTime() + 4 * 86400000) - event.getTime()) / (1000 * 60 * 60 * 24)) + 1;
-            this.setState({diferencaTempo: dif});
-            this.calculaPreco(this.state.tipo, undefined, dif)
+            this.setState({dataF: new Date(event.getTime() + 4 * 86400000)})
+            dif = Math.ceil(((event.getTime() + 4 * 86400000) - event.getTime()) / (1000 * 60 * 60 * 24)) + 1;
         } else {
             if (this.state.dataF < event.getTime()) {
                 this.setState({dataF: event})
-                const dif = Math.ceil((event.getTime() - event.getTime()) / (1000 * 60 * 60 * 24)) + 1;
-                this.setState({diferencaTempo: dif});
-                this.calculaPreco(this.state.tipo, undefined, dif)
+                dif = Math.ceil((event.getTime() - event.getTime()) / (1000 * 60 * 60 * 24)) + 1;
             } else {
-                const dif = Math.ceil((this.state.dataF.getTime() - event.getTime()) / (1000 * 60 * 60 * 24)) + 1;
-                this.setState({diferencaTempo: dif});
-                this.calculaPreco(this.state.tipo, undefined, dif)
+                dif = Math.ceil((this.state.dataF.getTime() - event.getTime()) / (1000 * 60 * 60 * 24)) + 1;
             }
         }
+        this.setState({diferencaTempo: dif});
+        this.calculaPreco(this.state.tipo, undefined, dif)
     }
 
     handleDataF(event) {
